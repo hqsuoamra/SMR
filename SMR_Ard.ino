3298c64e2ab4a20ba883e7c53a180cfe1c032cad
alobo2 <aplobo12@gmail.com>
2023-12-09 12:24:55 -0500

    Adding Arduino File for Ultrasonic Sensors

---
+++ SMR_Ard.ino
@@ -0,0 +1,6 @@
+ #define CUSTOM_SETTINGS
+#define INCLUDE_GAMEPAD_SHIELD
+#include <OneSheeld.h>
+ #include <PS2X_lib.h> 
+ /* A name for the LED on pin 4. */
+int ledPin = 4 ;
--- SMR_Sensor.py
+++ SMR_Sensor.py
@@ -19,7 +19,7 @@ time.sleep(0.00001)
 GPIO.output(TRIG, False)
 while GPIO.input(ECHO)==0:
     pulse_start = time.time()
-while GPIO. input (ECHO)==1:
+while GPIO.input (ECHO)==1:
     pulse_end = time.time() 
     pulse_duration = pulse_end - pulse_start
 distance = pulse_duration * 17150
